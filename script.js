a121385a43ba747112a54fa88bc046509e911fb9
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-03-18 00:10:27 +0530

    Added some JavaScript and Jquery elements

---
+++ script.js
@@ -0,0 +1,23 @@
+let ulTasks=$('#ulTasks')
+let btnAdd=$('#btnAdd')
+let btnClr=$('#btnClr')
+let inpNewTask=$('#inpNewTask')
+
+btnAdd.click(() => {
+    console.log(inpNewTask.val())
+    inpNewTask.val("")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
