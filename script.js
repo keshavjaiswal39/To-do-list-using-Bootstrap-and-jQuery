acf4cbad4d200f40dfa2defc4243ec93fe79bee6
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-03-18 00:33:44 +0530

    handle enter button

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let btnAdd=$('#btnAdd')
 let btnClr=$('#btnClr')
 let inpNewTask=$('#inpNewTask')
 
-// functionality for adding the items
-btnAdd.click(() => {
+function addItem()
+{
     let listItem=$('<li>',{
         'class': 'list-group-item',text:inpNewTask.val()
     })
@@ -16,7 +16,17 @@ btnAdd.click(() => {
 
     ulTasks.append(listItem)
     inpNewTask.val('')
+}
+
+inpNewTask.keypress((e) => {
+    if(e.which==13)
+    {
+        addItem()
+    }
 })
+
+// functionality for adding the items
+btnAdd.click(addItem)
 
 // functionalities for clearing the input field
 btnClr.click(() => {
